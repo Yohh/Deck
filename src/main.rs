969785e7b13eb0e385aa0c40228196f2ef0e4d6d
Yohh <durandyohan@zaclys.net>
2025-04-22 08:23:17 +0200

    add rust rand package, add impl to deck struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
+use rand::{rng, seq::SliceRandom};
+
+#[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let deck: Deck = Deck { cards: vec![] };
+impl Deck {
+    fn new() -> Self {
+        let suits = vec!["Hearts", "Diamonds", "Clubs", "Spades"];
+        let values = vec![
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace",
+        ];
 
-    println!("Here is the deck: {}", deck);
+        let mut cards = vec![];
+
+        for suit in &suits {
+            for value in &values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+
+    let hand = deck.deal(5);
+
+    println!("Here is the hand: {:#?}", hand);
 }
